a8b902f3ca5185c1303652cc937ac04c00c89e84
sachin <SHIVNERI@DESKTOP-POV7G28>
2024-04-06 10:32:40 +0530

    addition method created by lead

--- src/com/main/Addition.java
+++ src/com/main/Addition.java
@@ -6,6 +6,12 @@ public class Addition{
 		int a =10;
 		int b =20;
 		System.out.println("addition "+ (a+b));
+		
+	}
+	
+	public static void addition(int a, int b) {
+		
+		System.out.println("addtion method"+(a+b));
 	}
 
 }
