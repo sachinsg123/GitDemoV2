77ceea9d12c3a20e6ba9c0d69c0b952235478369
sachin <SHIVNERI@DESKTOP-POV7G28>
2024-04-06 13:07:05 +0530

    new commit

--- src/com/main/Addition.java
+++ src/com/main/Addition.java
@@ -2,7 +2,8 @@ package com.main;
 
 public class Addition{
 	
-	public static void main(String[] args) {
+	public static void main(String[] args){
+		
 		int a =10;
 		int b =20;
 		System.out.println("addition "+ (a+b));
