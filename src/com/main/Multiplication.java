4333834135c192f416423db30e89079e208bb0b6
sachin <SHIVNERI@DESKTOP-POV7G28>
2024-04-06 11:54:15 +0530

    multiplication commit from BranchX

---
+++ src/com/main/Multiplication.java
@@ -0,0 +1,13 @@
+package com.main;
+
+public class Multiplication {
+
+	public static void main(String[] args){
+		
+		int a =10;
+		int b = 20;
+		System.out.println("Multiplication of two numbers"+(a*b));
+
+	}
+
+}
